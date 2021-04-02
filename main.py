31aab7334ddc57e2d90861a6497c3d7aff974f3b
mhkhan <mehtabhabib@gmail.com>
2021-04-02 10:19:36 +0500

    6 testing files created and code added in main.py to read and count extensions of testing files

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
 import os
 
-print("Hello World")
+# Getting the current work directory (cwd)
+thisdir = os.getcwd()
+
+# r=root, d=directories, f = files
+# for r, d, f in os.walk(thisdir):
+#     for file in f:
+#         #if file.endswith(".docx"):
+#			# print complete file names 
+#         	print(os.path.join(r, file)) 
+
+ext_count = dict()
+ext_count = {'No_Extension':0}
+filenames = next(os.walk(thisdir))[2]
+print(filenames)
+
+for file in filenames:
+	ext=file.split(".")[-1]
+	#print(ext)
+	if ext in ext_count:
+		ext_count[ext]+= 1
+		#print(ext+" found")
+	elif ext==file:
+		ext_count['No_Extension']+=1
+		#print("no extension")	
+	else:
+		ext_count[ext]= 1
+		#print(ext+" not found")
+
+print (ext_count)
