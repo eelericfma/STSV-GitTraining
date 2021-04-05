e0926e03a709f585f036cc27f40a9895cc42ba92
DESKTOP-DNDKKN5\IDREESIA <mrwicky381@gmail.com>
2021-04-05 08:22:55 +0500

    webscrapping tool use beautifulsoup

--- main.py
+++ main.py
@@ -1,18 +1,47 @@
-import os
-import glob
-path = 'C:/Users/IDREESIA/Documents/STSV-GitTraining/'
-for entry in os.listdir(path):
-    if  os.path.isfile(os.path.join(path, entry)):
-        print(entry)
-
-from os.path import join, splitext
-from glob import glob
-from collections import Counter
-
-path = r'C:/Users/IDREESIA/Documents/STSV-GitTraining/'
-
-c = Counter([splitext(i)[1][1:] for i in glob(join(path, '*'))])
-for ext, count in c.most_common():
-    print (ext, count)
-
-print("Flashers","Solid State Relays","Pulse Relays","Time Relays","Toggle Relays","Speial Relays", "Voltage Monitors","Frequency Monitors","Standard Relays")
+import requests
+from bs4 import BeautifulSoup
+url = "https://www.mrs-electronic.com/en/products/relays"
+#get the html
+r = requests.get(url , verify=False)
+htmlContent = r.content
+#print(htmlContent)
+#Parse the html
+soup = BeautifulSoup(htmlContent, 'html.parser')
+print(soup.prettify)
+#html tree traversal
+#common use type of objects
+#print(type(title)) #tags
+#print(type(title.string)) #navigable string
+#print(type(soup)) #beautiful soup
+#comment
+markup ="<p><!--this is a comment --></p>"
+soup2 = BeautifulSoup(markup)
+print(soup2.p)
+print(soup2.p.string)
+print(type(soup2.p.string))
+#exit()
+#Get the title of html
+title = soup.title
+print(title)
+#find all paragraph of the page
+paras = soup.find_all('p')
+print(paras)
+#find all achor tags from the page
+achors = soup.find_all('a')
+#all_links = set() #empty set
+print(achors)
+#get all the links on the page
+#for link in achors:
+    #print(link.get('href'))
+#    if(link.get('href') != '#'):
+#     linkText = "https://www.mrs-electronic.com" +link.get('href')
+#      all_links.add(link)
+#       print(linkText) 
+#find first element paragraph of the page
+print(soup.find('p')) 
+#find first element of paragraph of the class of page
+#print(soup.find('p')['class'])
+#find all the elements  with class name
+#print(soup.find_all("div", class_="page"))
+#get a text from tags/soups
+print(soup.find('p').get_text()) 
